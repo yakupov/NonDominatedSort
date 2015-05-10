2bdc7a01892713daf96c3fde1a9f1789a2827d42
IlyaY <iyakupov93@gmail.com>
2015-05-10 20:51:02 +0300

    proof added

--- ImaMaster/clean.bat
+++ ImaMaster/clean.bat
@@ -1,3 +1,12 @@
+pwd
+cd pic
+pwd
+for %%a in (1 2 3 4 5 6 8 9 10 mpx log) do (
+    if exist *.%%a del *.%%a
+)
+cd ..
+pwd
+
 for %%f in (main.*) do (
 	if not [%%f] == [main.bib] if not [%%f] == [main.tex] del %%f
 )
--- ImaMaster/compile.bat
+++ ImaMaster/compile.bat
@@ -1,4 +1,11 @@
 call clean
+
+cd pic
+for %%i in (*.mp) do (
+    "C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\mpost" -tex=latex %%i
+)
+cd ..
+
 "C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\miktex-pdftex.exe" -undump=pdflatex main.tex
 "C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\biber.exe" main
 "C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\miktex-pdftex.exe" -undump=pdflatex main.tex
